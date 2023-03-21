725658b6d3fd3bdd3b86b113c8c9f2ced76d2702
GiselaFlores <gi.flores@bue.edu.ar>
2023-03-20 22:04:46 -0300

    links y metodos

--- arrays.js
+++ arrays.js
@@ -1,39 +1,45 @@
 const numeros = [1, 2, 3, 4, 5, 6, 7, 8];
-const ul = document.querySelector("ul");
+ul = document.querySelector("ul");
 
+//ForEach
 numeros.forEach((num)=>{
 
-    const li =document.createElement("li");
+    const li = document.createElement("li");
     li.textContent = num;
-
     ul.appendChild(li);
-    
 });
 
-const multiplosDeDiez = numeros.map(multi => multi*10)
+//Map
+const multiplosDeDiez = numeros.map(multi => multi*50);
 console.log(multiplosDeDiez);
 
-const multiplosDeDiez2 = numeros.map((multiDos) => {
-    
-    const li =document.createElement("li");
-    let resultado = multiDos*10;
+const multiplosDeDiez2 = numeros.map((multiDos) =>{
 
+    let resultado = multiDos;
+    const li = document.createElement("li");
     li.textContent = resultado;
-
     ul.appendChild(li);
 
     return resultado;
-    
-    });
+});
 
 console.log(multiplosDeDiez2);
 
-const pares = numeros.filter(num => num>2 && num<5);
-console.log(pares);
 
+/* Metodos
 
-const inverso = numeros.sort((a, b)=> a>b ? -1:1);
-console.log(inverso);
+https://tutobasico.com/metodos-arrays-javascript/
 
+https://francescricart.com/metodos-javascript/
 
+https://es.javascript.info/array-methods
 
+*/ 
+const entreNum = numeros.filter(numDos => numDos>2 && numDos<6);
+console.log(entreNum);
+
+const contadorUnico = numeros.reduce((acumulador, itera)=>acumulador + itera);
+console.log(contadorUnico);
+
+const orden = numeros.sort((a, b)=> a>b ? -1:1);
+console.log(orden);
